9327b06220603524590e3983934782378fde5454
webklad <102153384+webklad@users.noreply.github.com>
2022-04-11 15:38:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const modalWindow = document.querySelector(".modal");
-const buttonsModal = document.querySelectorAll(".button-play");
+const modalWindow = document.querySelector('.modal');
+const buttonsModal = document.querySelectorAll('.button-play');
 const iframe = document.querySelector('iframe')
 
 buttonsModal.forEach((item, i) => {
-  item.addEventListener("click", () => {
-    modalWindow.classList.add("active");
+  item.addEventListener('click', () => {
+    modalWindow.classList.add('active');
   });
 });
 
-modalWindow.addEventListener("click", () => {
-  modalWindow.classList.remove("active");
-  document.querySelector("iframe").remove();
-  document.querySelector(".modal__inner").append(iframe);
+modalWindow.addEventListener('click', () => {
+  modalWindow.classList.remove('active');
+  document.querySelector('iframe').remove();
+  document.querySelector('.modal__inner').append(iframe);
 });
